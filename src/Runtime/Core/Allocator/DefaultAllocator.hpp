7e290346f6366705063b08c397cdc95797462ebf
mos9527 <greats3an@gmail.com>
2025-09-19 08:29:51 +0800

    Clean up

--- src/Runtime/Core/Allocator/DefaultAllocator.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "HeapAllocator.hpp"
-namespace Foundation::Core {
-    /**
-     * @brief Alias for HeapAllocator with tracking enabled.
-     */
-    using DefaultAllocator = HeapAllocator<true>;
-}
--- src/Runtime/Core/Core.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Allocator/Allocator.hpp"
-#include "Container/Common.hpp"
-#include "Logging/Logging.hpp"
-
-/**
- * @brief Allocators, Data Structures and introspection implementations.
- */
-namespace Foundation::Core {}
