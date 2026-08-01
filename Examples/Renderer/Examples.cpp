5d23df56c34a868602529495ef78ad1e31783632
mos9527 <komahuang@tencent.com>
2026-08-01 11:20:46 +0800

    Dithered alpha for GBuffer gen

--- Examples/Renderer/Examples.cpp
+++ Examples/Renderer/Examples.cpp
@@ -9,7 +9,7 @@
 bool Examples_RendererSwitchButton(ExampleInputState& input, ExampleRenderer& currentRenderer)
 {
     bool changed = false;    
-    if (Examples_Button(input, Format("[{}]", currentRenderer)))
+    if (Examples_Button(input, Format(">> Current Renderer: [{}] <<", currentRenderer)))
     {
         currentRenderer = static_cast<ExampleRenderer>((static_cast<uint32_t>(currentRenderer) + 1) %
                                                        static_cast<uint32_t>(ExampleRenderer::Count));
