6ef564c2546bbb8f696ae5781e17fe8cce2cf478
mos9527 <komahuang@tencent.com>
2026-09-21 13:28:06 +0800

    Fix CI 2/N

--- Source/Platform/LoggingPosix.cpp
+++ Source/Platform/LoggingPosix.cpp
@@ -1,13 +1,16 @@
 #include "Logging.hpp"
 
+#include <Core/Container.hpp>
+
 #include <cstdio>
 
 namespace Foundation::Platform {
 
 void WriteLog(LogLevel level, const char* tag, double seconds, const char* formatted)
 {
-    auto str = Format("{}|+{:>5.5f}s|{} {}", level, seconds, tag, formatted);
+    auto str = Foundation::Core::Format("{}|+{:>5.5f}s|{} {}", level, seconds, tag, formatted);
     fprintf(stderr, "%s\n", str.c_str());
+    fflush(stderr);
 }
 
 void Print(const char* formatted, bool flush)
